2a9a2cfce6f5cabcb1165ee480b27d6a421647ef
apostolicfaithchurchisiwuyouth <apostolicfaithchurchisiwuyouth@gmail.com>
2026-08-10 09:09:24 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(registration => {
+        console.log(
+          "AFC Isiu PWA: Service worker registered.",
+          registration.scope
+        );
+      })
+      .catch(error => {
+        console.error(
+          "AFC Isiu PWA: Service worker registration failed.",
+          error
+        );
+      });
+  });
+}
 
 AOS.init({
     duration: 900,
